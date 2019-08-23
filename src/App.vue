923a59c83c38dd12f3fd44045db2310fb13abde0
Igor Bailov <baylovid@gmail.com>
2019-08-23 20:28:59 +0300

    Added Dashboard and diagrams

--- src/App.vue
+++ src/App.vue
@@ -9,27 +9,27 @@
     <aside id="aside" class="ui-aside">
       <ul class="nav" ui-nav>
         <li>
-          <a href="index.html"><i class="fa fa-home"></i><span> Dashboard </span></a>
+          <router-link to="/"><i class="fa fa-home"></i><span> Dashboard </span></router-link>
           <ul class="nav nav-sub">
-            <li class="nav-sub-header"><a href="index.html"><span>Dashboard</span></a></li>
+            <router-link to="/" class="nav-sub-header"><a><span>Dashboard</span></a></router-link>
           </ul>
         </li>
         <li>
-          <a href="#"><i class="fa fa-file-text"></i><span>Contracts</span></a>
+          <router-link to="/contracts"><i class="fa fa-file-text"></i><span>Contracts</span></router-link>
           <ul class="nav nav-sub">
-            <li class="nav-sub-header"><a href="#"><span>Contracts</span></a></li>
+            <router-link to="/contracts" class="nav-sub-header"><a href="#"><span>Contracts</span></a></router-link>
           </ul>
         </li>
         <li>
-          <a href="#"><i class="fa fa-credit-card"></i><span> Accounts </span></a>
+          <router-link to="/accounts"><i class="fa fa-credit-card"></i><span> Accounts </span></router-link>
           <ul class="nav nav-sub">
-            <li class="nav-sub-header"><a href="#"><span>Accounts</span></a></li>
+            <router-link to="/accounts" class="nav-sub-header"><a href="#"><span>Accounts</span></a></router-link>
           </ul>
         </li>
         <li>
-          <a href="#"><i class="fa fa-list-alt"></i><span> Acts </span></a>
+          <router-link to="/acts"><i class="fa fa-list-alt"></i><span> Acts </span></router-link>
           <ul class="nav nav-sub">
-            <li class="nav-sub-header"><a href="#"><span>Acts</span></a></li>
+            <router-link to="/acts" class="nav-sub-header"><a href="#"><span>Acts</span></a></router-link>
           </ul>
         </li>
         <li>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="row">
+        <div class="col-md-6">
+              <div class="panel">
+                  <header class="panel-heading">
+                    Pie Chart
+                    <span class="tools pull-right">
+                        <a class="refresh-box fa fa-repeat" href="javascript:;"></a>
+                        <a class="collapse-box fa fa-chevron-down" href="javascript:;"></a>
+                        <a class="close-box fa fa-times" href="javascript:;"></a>
+                    </span>
+                  </header>
+                  <div class="panel-body">
+                      <div class="chartJS">
+                          <canvas id="pie-chart-js" height="250" width="800" ></canvas>
+                      </div>
+                  </div>
+              </div>
+        </div>
+        <div class="col-md-6">
+              <div class="panel">
+                    <header class="panel-heading">
+                      Donut Chart
+                      <span class="tools pull-right">
+                          <a class="refresh-box fa fa-repeat" href="javascript:;"></a>
+                          <a class="collapse-box fa fa-chevron-down" href="javascript:;"></a>
+                          <a class="close-box fa fa-times" href="javascript:;"></a>
+                      </span>
+                    </header>
+                    <div class="panel-body">
+                        <div class="chartJS">
+                            <canvas id="donut-chart-js" height="250" width="800" ></canvas>
+                        </div>
+                    </div>
+              </div>
+        </div>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'dashboard'
+}
+</script>
